346946d15119cd9ecde4ee4ebfceca015ff504e8
Denis Rodionov <den.iu7@gmail.com>
2021-01-24 23:01:32 +0100

    UpdatedAt added to the database

--- dynamodb.py
+++ dynamodb.py
@@ -1,10 +1,13 @@
 import boto3
 import uuid
+import calendar
+import time
 
 def create_project(project):
     print("DynamoDB: saving project:", project['title'])
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('pl_projects')
     project['id'] = str(uuid.uuid4())
+    project['createdAt'] = calendar.timegm(time.gmtime())   # utc timestamp in sec
     response = table.put_item(Item=project)
     print(response)
